93d2029f231b4a97198ab8d05f6ba91a8800c02e
Merve <htc_mrv@hotmail.com>
2020-08-18 16:58:00 -0400

    first step

---
+++ src/main/java/Utilities/Driver.java
@@ -0,0 +1,26 @@
+package Utilities;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+
+        WebDriverManager.chromedriver().setup();
+
+        return driver;
+    }
+
+
+}
